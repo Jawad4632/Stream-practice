e460d8a59bae2edb73673afe25c86f92daedee9c
jawad23333 <jawad@correctcloud.io>
2025-02-26 15:07:39 +0530

    rename from main to BasicOfStreams

--- src/main/java/com/example/BasicOfStreams.java
+++ src/main/java/com/example/BasicOfStreams.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.List;
 import java.util.function.*;
 
-public class Main {
+public class BasicOfStreams {
     public static void main(String[] args) {
 
         // functional programming
