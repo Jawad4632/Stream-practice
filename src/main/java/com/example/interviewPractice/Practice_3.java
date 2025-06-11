5e43126d321b9de6025b98caa0db2e69b57b6dae
Jawad4632 <jk8208486@gmail.com>
2025-06-11 22:30:49 +0530

    Practice 3 question are added for solving

---
+++ src/main/java/com/example/interviewPractice/Practice_3.java
@@ -0,0 +1,21 @@
+package com.example.interviewPractice;
+
+public class Practice_3 {
+    public static void main(String[] args){
+//➡️ Java 8 Program to Remove Duplicate Elements from a List
+//➡️ Reverse Each Word of String using Java 8 Stream
+//➡️ Java 8 Program to Find the Second Largest Number in the List of Integers
+//➡️ Java 8 Program to Find the Maximum Number and Minimum Number in a List
+//➡️ Sort Employee Objects by Salary using Lambda
+//➡️ Java 8 Program to Find the Sum of All Digits of a Number
+//➡️ Java 8 Program to Find the Frequency of Each Character in a Given String
+//➡️ Java 8 Program to Find the Maximum Number and Minimum Number in a List
+//➡️ Java 8 Program to Print Even Numbers from a List
+//➡️ Find the Square of the First Three Even Numbers using Java     8 Stream
+//➡️ Find the Average of a Given List of Numbers using Java 8 Stream
+//➡️ Convert a List of Strings to Uppercase and Lowercase using Java 8 Stream
+//➡️ Collectors groupingBy - Group Employees By Age
+//➡️ Java 8 Program to Find the Age of a Person if the Birthday Date has Given
+//➡️ Sort the List of Strings in Ascending and Descending Order using Java 8 Stream
+    }
+}
